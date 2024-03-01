fe042bd8f46a3cfee1b0113607ec4d94cb564eee
dean <nicklasfrederiksen@gmail.com>
2024-03-01 14:12:14 +0100

    First

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.introduction"
-    compileSdk = 34
+        compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.introduction"
--- app/src/main/java/com/example/introduction/MainActivity.kt
+++ app/src/main/java/com/example/introduction/MainActivity.kt
@@ -3,53 +3,89 @@ package com.example.introduction
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.introduction.ui.theme.IntroductionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             IntroductionTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Hello Android")
+                    Layout();
                 }
             }
+
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    //Column 2 rows
+    Column{
+        Text("Hello $name", modifier = Modifier);
+        Text("Hello $name", modifier = Modifier);
+        Text("Hello $name", modifier = Modifier);
+    }
+}
+
+//Column 2 rows
+@Composable
+fun Layout(modifier: Modifier = Modifier){
+    Column{
+        Row{
+            Box(modifier = Modifier
+                .background(Color.Red)
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth(0.5f),
+                contentAlignment = Alignment.BottomCenter
+            ){
+                Text("Hey allesammen");
+            };
+            Box(modifier = Modifier
+                .background(Color.Blue)
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth()
+            ){};
+        }
+
+        Row{
+            Box(modifier = Modifier
+                .background(Color.Yellow)
+                .fillMaxHeight()
+                .fillMaxWidth(0.5f)
+            ){};
+            Box(modifier = Modifier
+                .background(Color.Green)
+                .fillMaxHeight()
+                .fillMaxWidth()
+            ){};
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    IntroductionTheme {
-        Greeting("Android")
-    }
+    Layout();
 }
